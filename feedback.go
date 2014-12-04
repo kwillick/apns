179cca58c988324d5c57791b752e029a3aeefac1
Kipp Hickman <spud.kipp@gmail.com>
2014-12-04 14:30:45 -0800

    Rewrote feedback.go to not depend on client.go. When listening for feedback, the connection will no longer timeout. Removed client.go.

--- feedback.go
+++ feedback.go
@@ -6,96 +6,121 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"errors"
+	"fmt"
+	"io"
 	"net"
-	"strings"
 	"time"
 )
 
-// Wait at most this many seconds for feedback data from Apple.
-const FeedbackTimeoutSeconds = 5
+var AppleFeedbackProductionGateway = Gateway{
+	Host: "feedback.push.apple.com",
+	Port: "2196",
+}
 
-// FeedbackChannel will receive individual responses from Apple.
-var FeedbackChannel = make(chan (*FeedbackResponse))
-
-// If there's nothing to read, ShutdownChannel gets a true.
-var ShutdownChannel = make(chan bool)
+var AppleFeedbackDevelopmentGateway = Gateway{
+	Host: "feedback.sandbox.push.apple.com",
+	Port: "2196",
+}
 
 // FeedbackResponse represents a device token that Apple has
 // indicated should not be sent to in the future.
 type FeedbackResponse struct {
-	Timestamp   uint32
+	Timestamp   time.Time
 	DeviceToken string
 }
 
-// NewFeedbackResponse creates and returns a FeedbackResponse structure.
-func NewFeedbackResponse() (resp *FeedbackResponse) {
-	resp = new(FeedbackResponse)
-	return
+func FeedbackResponseFromBytes(data []byte) (*FeedbackResponse, error) {
+	byteReader := bytes.NewReader(data[0:4])
+	var timestamp uint
+	if err := binary.Read(byteReader, binary.BigEndian, &timestamp); err != nil {
+		return nil, err
+	}
+
+	byteReader = bytes.NewReader(data[5:7])
+	var length uint16
+	if err := binary.Read(byteReader, binary.BigEndian, &length); err != nil {
+		return nil, err
+	}
+	if length != 32 {
+		return nil, errors.New(fmt.Sprintf("Expected device token length of 32, got '%v'", length))
+	}
+
+	response := &FeedbackResponse{
+		Timestamp:   time.Unix((int64)(timestamp), 0),
+		DeviceToken: hex.EncodeToString(data[6:]),
+	}
+	return response, nil
+}
+
+// Timestamp + Length + Device Token
+const feedbackResponseSizeBytes = 4 + 2 + 32
+
+type FeedbackConnection struct {
+	Gateway   string
+	tlsConfig tls.Config
+	tlsConn   *tls.Conn
+}
+
+func NewFeedbackConnection(gateway Gateway, certificateFile string, keyFile string) (*FeedbackConnection, error) {
+	cert, certErr := tls.LoadX509KeyPair(certificateFile, keyFile)
+	if certErr != nil {
+		return nil, certErr
+	}
+
+	fc := &FeedbackConnection{
+		Gateway: net.JoinHostPort(gateway.Host, gateway.Port),
+		tlsConfig: tls.Config{
+			Certificates: []tls.Certificate{cert},
+			ServerName:   gateway.Host,
+		},
+	}
+	return fc, nil
 }
 
 // ListenForFeedback connects to the Apple Feedback Service
 // and checks for device tokens.
-//
-// Feedback consists of device tokens that should
-// not be sent to in the future; Apple *does* monitor that
-// you respect this so you should be checking it ;)
-func (client *Client) ListenForFeedback() (err error) {
-	var cert tls.Certificate
-
-	if len(client.CertificateBase64) == 0 && len(client.KeyBase64) == 0 {
-		// The user did not specify raw block contents, so check the filesystem.
-		cert, err = tls.LoadX509KeyPair(client.CertificateFile, client.KeyFile)
-	} else {
-		// The user provided the raw block contents, so use that.
-		cert, err = tls.X509KeyPair([]byte(client.CertificateBase64), []byte(client.KeyBase64))
+func (fc *FeedbackConnection) ListenForFeedback(responseChannel chan *FeedbackResponse) error {
+	if responseChannel == nil {
+		return errors.New("Got a nil response channel")
 	}
 
+	var err error
+	fc.tlsConn, err = tls.Dial("tcp", fc.Gateway, &fc.tlsConfig)
 	if err != nil {
+		close(responseChannel)
 		return err
 	}
 
-	gatewayParts := strings.Split(client.Gateway, ":")
-	conf := &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		ServerName: gatewayParts[0],
-	}
-
-	conn, err := net.Dial("tcp", client.Gateway)
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	conn.SetReadDeadline(time.Now().Add(FeedbackTimeoutSeconds * time.Second))
-
-	tlsConn := tls.Client(conn, conf)
-	err = tlsConn.Handshake()
-	if err != nil {
-		return err
-	}
-
-	var tokenLength uint16
-	buffer := make([]byte, 38, 38)
-	deviceToken := make([]byte, 32, 32)
-
+	fc.tlsConn.SetReadDeadline(time.Time{})
+	buffer := make([]byte, feedbackResponseSizeBytes)
 	for {
-		_, err := tlsConn.Read(buffer)
+		_, readErr := fc.tlsConn.Read(buffer)
+		if readErr != nil {
+			if readErr == io.EOF {
+				// No data available. Sleep for 30 minutes before trying again
+				fc.tlsConn.Close()
+				time.Sleep(time.Duration(30) * time.Minute)
+
+				// Reconnect
+				fc.tlsConn, err = tls.Dial("tcp", fc.Gateway, &fc.tlsConfig)
+				if err != nil {
+					close(responseChannel)
+					return err
+				}
+				continue
+			}
+
+			close(responseChannel)
+			return readErr
+		}
+
+		response, err := FeedbackResponseFromBytes(buffer)
 		if err != nil {
-			ShutdownChannel <- true
-			break
+			close(responseChannel)
+			return err
 		}
 
-		resp := NewFeedbackResponse()
-
-		r := bytes.NewReader(buffer)
-		binary.Read(r, binary.BigEndian, &resp.Timestamp)
-		binary.Read(r, binary.BigEndian, &tokenLength)
-		binary.Read(r, binary.BigEndian, &deviceToken)
-		if tokenLength != 32 {
-			return errors.New("token length should be equal to 32, but isn't")
-		}
-		resp.DeviceToken = hex.EncodeToString(deviceToken)
-
-		FeedbackChannel <- resp
+		responseChannel <- response
 	}
 
 	return nil
